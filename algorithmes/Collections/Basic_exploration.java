f6fca406ec8011347e616fa9d7c94abd740722d0
usb777 <dzsamoila@gmail.com>
2021-03-16 18:04:00 -0400

    Java Collections

---
+++ algorithmes/Collections/Basic_exploration.java
@@ -0,0 +1,35 @@
+package algorithmes.Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Basic_exploration {
+
+	public static void main(String[] args) 
+	{
+	
+		
+		List<Integer> numbers =  new ArrayList<Integer>();
+		List<Integer> numbers1=  new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		//numbers1 = numbers;
+		numbers1.addAll(numbers); // main order
+		
+		System.out.println(numbers.toString());
+		
+		 Collections.shuffle(numbers);
+		
+		System.out.println(numbers.toString());
+		System.out.println(numbers1.toString());
+		
+		
+	
+
+	}
+
+}
