11decfaa88a6ff18f310e83c7ce988c2e39c0fe1
usb777 <dzsamoila@gmail.com>
2021-03-18 23:54:20 -0400

    Matrix

---
+++ interviews/matrix/MatrixClass.java
@@ -0,0 +1,53 @@
+package interviews.matrix;
+
+
+
+public class MatrixClass {
+
+	public static void main(String[] args) 
+	{
+	 
+	int matrix[][] = getMatrix(3,4);
+	
+	print2D(matrix);
+		
+	}
+	
+	
+	
+	
+	static int[][] getMatrix(int n, int m)
+	{   int[][] matrix = new int[n][m];
+		
+	  int max = 1; 
+      int min = 0; 
+      int range = max - min + 1; 
+      
+	    for (int i=0;i<n;i++)
+	    	{
+	    	for (int j=0;j<m;m++)
+	    		{
+	    		  matrix[i][j] = (int)(Math.random() * range) + min;
+	    		}
+	    	}
+		
+		return matrix;
+	}
+	
+	
+	 public static void print2D(int mat[][])
+	    {
+	        // Loop through all rows
+	        for (int i = 0; i < mat.length; i++)
+	        {
+	            // Loop through all elements of current row
+	            for (int j = 0; j < mat[i].length; j++)
+	            {  System.out.print(mat[i][j] + " ");}
+	            System.out.println();
+	        }
+	       
+	        }
+	        
+	
+
+}
