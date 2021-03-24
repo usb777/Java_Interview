4d285e0a6365f54f0e1ac33984d56586e6969fd2
usb777 <dzsamoila@gmail.com>
2021-03-23 20:43:04 -0400

    Binary

---
+++ interviews/numbers/BinaryDecimal.java
@@ -0,0 +1,21 @@
+package interviews.numbers;
+
+import java.util.Scanner;
+
+public class BinaryDecimal {
+
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter decimal number");
+		
+		String number = sc.nextLine();
+		
+		
+		System.out.println(number);
+		
+		
+	}
+
+}
