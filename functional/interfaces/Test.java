3c525abddb48c77bb9924d64327f66fb6d7515d7
usb777 <dzsamoila@gmail.com>
2021-04-08 14:36:14 -0400

    Functional Interfaces

--- functional/interfaces/Test.java
+++ functional/interfaces/Test.java
@@ -1,10 +1,43 @@
 package functional.interfaces;
 
-public class Test {
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ interface MyFunctionalInterface
+{
+    public void execute();
 
+    public static void doGet()
+    {
+    	  System.out.println("Hello");
+    }
+    
+    public default void print(String text) 
+    {
+        System.out.println(text);
+        
+        
+    }
+
+    public static void print(String text, PrintWriter writer) throws IOException {
+        writer.write(text);
+    }
+}
+
+public class Test implements MyFunctionalInterface {
+
+	public static void main(String[] args) 
+	{
+		MyFunctionalInterface lambda = () -> {    System.out.println("Executing...");		};
+		lambda.print("printus");
+		
+	}
+
+	@Override
+	public void execute() 
+	{
+		
+		
 	}
 
 }
